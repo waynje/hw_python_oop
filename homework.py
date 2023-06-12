a06619351881a6d6e1103e71a0c40e6a542830ee
Юрий Агеев <ageev.y@yandex.ru>
2023-06-12 22:51:36 +0300

    Add final version

--- homework.py
+++ homework.py
@@ -42,7 +42,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise Exception('Not usable function')
+        raise NotImplementedError
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -74,18 +74,18 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     height: float
-    CALORIES_MEAN_SPEED_MULTIPLIER_1 = 0.035
-    CALORIES_MEAN_SPEED_MULTIPLIER_2 = 0.029
+    CALORIES_WEIGHT_MULTIPLIER_1 = 0.035
+    CALORIES_WEIGHT_MULTIPLIER_2 = 0.029
     S_IN_HR = 3600
     CM_IN_M = 100
     SPEED_M_S = round(Training.M_IN_KM / S_IN_HR, 3)
 
     def get_spent_calories(self) -> float:
         return (
-            (self.CALORIES_MEAN_SPEED_MULTIPLIER_1 * self.weight
+            (self.CALORIES_WEIGHT_MULTIPLIER_1 * self.weight
              + ((self.get_mean_speed() * self.SPEED_M_S) ** 2
                 / (self.height / self.CM_IN_M))
-             * self.CALORIES_MEAN_SPEED_MULTIPLIER_2 * self.weight)
+             * self.CALORIES_WEIGHT_MULTIPLIER_2 * self.weight)
             * (self.duration * self.M_IN_HR)
         )
 
@@ -136,9 +136,10 @@ def read_package(workout_type: str, data: list[int]) -> Training:
         raise ValueError(TRAINING_TYPE_ERROR.format(workout_type=workout_type))
     workout, length = TRAINING_TYPES[workout_type]
     if len(data) != length:
-        raise ValueError(PACKAGE_LENGTH_ERROR.format(workout_type=workout_type,
-                                                     package_length=len(data),
-                                                     normal_length=length))
+        raise ValueError(PACKAGE_LENGTH_ERROR.format(
+            workout_type=workout_type,
+            package_length=len(data),
+            normal_length=length))
     return workout(*data)
 
 
