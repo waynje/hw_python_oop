8c5d0edc3c22878bb19e543305a95795dcd20ee9
Юрий Агеев <ageev.y@yandex.ru>
2023-06-12 17:39:24 +0300

    Fix second review

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, fields
 
 
 @dataclass
@@ -10,16 +10,16 @@ class InfoMessage:
     speed: float
     calories: float
 
-    message: str = ('Тип тренировки: {training_type}; '
-                    'Длительность: {duration:.3f} ч.; '
-                    'Дистанция: {distance:.3f} км; '
-                    'Ср. скорость: {speed:.3f} км/ч; '
-                    'Потрачено ккал: {calories:.3f}.'
-                    )
+    MESSAGE = ('Тип тренировки: {training_type}; '
+               'Длительность: {duration:.3f} ч.; '
+               'Дистанция: {distance:.3f} км; '
+               'Ср. скорость: {speed:.3f} км/ч; '
+               'Потрачено ккал: {calories:.3f}.'
+               )
 
     def get_message(self) -> str:
         """Выводим информацию о тренировке."""
-        return self.message.format(**asdict(self))
+        return self.MESSAGE.format(**asdict(self))
 
 
 @dataclass
@@ -28,7 +28,6 @@ class Training:
     M_IN_KM = 1000
     M_IN_HR = 60
     LEN_STEP = 0.65
-    """Расстояние за один шаг."""
     action: int
     duration: float
     weight: float
@@ -43,7 +42,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise Exception('Not usable function')
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -75,19 +74,18 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     height: float
-    WEIGHT_KOEF = 0.035
-    WEIGHT_KOEF_2 = 0.029
+    CALORIES_MEAN_SPEED_MULTIPLIER_1 = 0.035
+    CALORIES_MEAN_SPEED_MULTIPLIER_2 = 0.029
     S_IN_HR = 3600
-    M_IN_KM = 1000
     CM_IN_M = 100
-    SPEED_M_S = round(M_IN_KM / S_IN_HR, 3)
+    SPEED_M_S = round(Training.M_IN_KM / S_IN_HR, 3)
 
     def get_spent_calories(self) -> float:
         return (
-            (self.WEIGHT_KOEF * self.weight
+            (self.CALORIES_MEAN_SPEED_MULTIPLIER_1 * self.weight
              + ((self.get_mean_speed() * self.SPEED_M_S) ** 2
                 / (self.height / self.CM_IN_M))
-             * self.WEIGHT_KOEF_2 * self.weight)
+             * self.CALORIES_MEAN_SPEED_MULTIPLIER_2 * self.weight)
             * (self.duration * self.M_IN_HR)
         )
 
@@ -96,9 +94,8 @@ class SportsWalking(Training):
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP = 1.38
-    SPEED_KOEF = 1.1
-    WEIGHT_KOEF = 2
-    """Расстояние за один гребок."""
+    CALORIES_MEAN_SPEED_SHIFT = 1.1
+    CALORIES_WEIGHT_MULTIPLIER = 2
     length_pool: float
     count_pool: float
 
@@ -110,25 +107,39 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         return (
-            (self.get_mean_speed() + self.SPEED_KOEF) * self.WEIGHT_KOEF
+            (self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
+            * self.CALORIES_WEIGHT_MULTIPLIER
             * self.weight * self.duration
         )
 
 
 TRAINING_TYPES = {
-    'RUN': Running,
-    'SWM': Swimming,
-    'WLK': SportsWalking
+    'RUN': (Running, len(fields(Running))),
+    'SWM': (Swimming, len(fields(Swimming))),
+    'WLK': (SportsWalking, len(fields(SportsWalking)))
 }
+
+TRAINING_TYPE_ERROR = (
+    'Тренировки "{workout_type}" нет в базе тренировок'
+)
+
+PACKAGE_LENGTH_ERROR = (
+    'Неверное количество параметров для "{workout_type}": {package_length}, '
+    'Должно быть: {normal_length} '
+)
 
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    if workout_type in TRAINING_TYPES:
-        return TRAINING_TYPES[workout_type](*data)
-    else:
-        raise ValueError('Несуществующий код тренировки')
+    if workout_type not in TRAINING_TYPES:
+        raise ValueError(TRAINING_TYPE_ERROR.format(workout_type=workout_type))
+    workout, length = TRAINING_TYPES[workout_type]
+    if len(data) != length:
+        raise ValueError(PACKAGE_LENGTH_ERROR.format(workout_type=workout_type,
+                                                     package_length=len(data),
+                                                     normal_length=length))
+    return workout(*data)
 
 
 def main(training: Training) -> None:
